99c9e6a6c61fd15fd8ee24097d46d5a698a63774
Vytenis domasevicius <vytenis.domasevicius@vka.lt>
2020-02-07 10:57:20 +0200

    Version 1

---
+++ objektinis1Uzd.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string introduction = " Sveikas, ";
+    string name;
+    cin>>name;
+    int n = name.length() + introduction.length() + 5;
+    char pirma[n];
+    char antra[n+1];
+    for(int i = 0; i < n+1; i++){
+        pirma[i] = '*';
+        if(i>0 && i<n){
+            antra[i] = ' ';
+        }
+    }
+    antra[0] = '*';
+    antra[n] = '*';
+    antra[n+1] = ' ';
+    antra[n+2] = ' ';
+
+    string trecia = "* " + introduction + name + "!  *";
+
+
+
+
+    cout<<pirma<<endl;
+    cout<<antra<<endl;
+    cout<<trecia<<endl;
+    cout<<antra<<endl;
+    cout<<pirma<<endl;
+}
